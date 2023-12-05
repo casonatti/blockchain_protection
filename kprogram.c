34f110de2d36d8f5ae779f66eb20e627da98c232
Jeison <jeison.casonatti@gmail.com>
2023-12-05 14:30:16 -0300

    adding counter for access (half done)

--- kprogram.c
+++ kprogram.c
@@ -3,21 +3,24 @@
 #include <linux/fs.h>
 #include <uapi/linux/ptrace.h>
 
+BPF_HASH(counter_table);
 BPF_ARRAY(inode_map, u32, 1);
 BPF_PERF_OUTPUT(output);
 
 struct data_t {
-  u32 uid;
+  u64 uid;
   u32 pid;
   u32 hooked_inode;
   u32 protected_inode;
   u64 timestamp;
+  u64 counter;
   char message[18];
 };
 
 int protected_file(struct pt_regs *ctx, struct file *file) {
   struct data_t data = {};
   int index = 0;
+  u64 counter = 0;
 
   data.hooked_inode = file->f_inode->i_ino;             //pega o inode do arquivo que esta sendo aberto 
 
@@ -27,8 +30,15 @@ int protected_file(struct pt_regs *ctx, struct file *file) {
   unsigned int *inode_ptr = inode_map.lookup(&index);   //pega o endereço do inode do arquivo protegido no map de inodes (inserido no userspace)
   if(inode_ptr != NULL) {
     data.protected_inode = *inode_ptr;                  //se existe um endereço, armazena o inode na estrutura de dados
-
     if(data.protected_inode == data.hooked_inode) {     //compara o inode do arquivo protegido com o inode do arquivo que esta sendo aberto
+      data.timestamp = bpf_ktime_get_ns();              //registra o timestamp do evento
+      u64 *counter_ptr = counter_table.lookup(&data.uid);
+      if(counter_ptr != 0) {
+        counter = *counter_ptr;
+        counter++;
+        counter_table.update(&data.uid, &counter);
+        data.counter = counter;
+      }
       if(data.uid == 1000) {                            //caso seja um usuario nao autorizado, cria a mensagem de negacao e mata o processo
         char message[18] = "Permission Denied";
         bpf_probe_read_kernel(&data.message, sizeof(data.message), message); //helper function usada para ler dados do kernel e armazena no espaco do ebpf
@@ -37,8 +47,6 @@ int protected_file(struct pt_regs *ctx, struct file *file) {
         char message[11] = "Authorized";
         bpf_probe_read_kernel(&data.message, sizeof(data.message), message); //helper function usada para ler dados do kernel e armazena no espaco do ebpf
       }
-
-      data.timestamp = bpf_ktime_get_ns();              //registra o timestamp do evento
 
       output.perf_submit(ctx, &data, sizeof(data));     //popula o buffer que pode ser lido no userspace
     }
--- uprogram.py
+++ uprogram.py
@@ -10,13 +10,13 @@ b.attach_kprobe(event = "vfs_write", fn_name= "protected_file")
 
 def print_event(cpu, data, size):
   data = b["output"].event(data)
-  print(f"{data.pid} {data.uid} {data.protected_inode} {data.hooked_inode} {data.message}")
+  print(f"{data.pid} {data.uid} {data.message} {data.counter}")
 
 i_map = b["inode_map"]
 
-i_map[c_int(0)] = c_uint32(5406469) #TODO: flexibilizar o arquivo protegido
+i_map[c_int(0)] = c_uint32(5112459) #TODO: flexibilizar o arquivo protegido
 
-print("%-6s %-6s %-10s %-10s %-20s" % ("PID", "UID", "P_INODE", "H_INODE", "MESSAGE"))
+print("%-6s %-6s %-10s %-6s" % ("PID", "UID", "MESSAGE", "COUNTER"))
 
 b["output"].open_perf_buffer(print_event)
 
